6941590991a1433cbca6fde81fe64c80526b5019
Luke Puppo <puppol@oregonstate.edu>
2018-11-30 14:51:24 -0800

    Removed side ways movement from paddles

--- public/static/game.js
+++ public/static/game.js
@@ -25,14 +25,8 @@ var movement = {
 }
 document.addEventListener('keydown', function(event) {
   switch (event.keyCode) {
-    case 65: // A
-      movement.left = true;
-      break;
     case 87: // W
       movement.up = true;
-      break;
-    case 68: // D
-      movement.right = true;
       break;
     case 83: // S
       movement.down = true;
@@ -41,14 +35,8 @@ document.addEventListener('keydown', function(event) {
 });
 document.addEventListener('keyup', function(event) {
   switch (event.keyCode) {
-    case 65: // A
-      movement.left = false;
-      break;
     case 87: // W
       movement.up = false;
-      break;
-    case 68: // D
-      movement.right = false;
       break;
     case 83: // S
       movement.down = false;
